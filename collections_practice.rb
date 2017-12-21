70a722617483e84285ebe82e76e02deba19ab2a0
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-21 01:42:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+
+def swap_elements(array)
+  list = "bsa"
+  array.sort_by do |word|
+    word.split('').map do |letter|
+      list.index(letter)
+    end
+  end
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+def kesha_maker(array)
+  array_new = []
+  array.each do |word|
+    array_new << word[0..1] + (word[2].replace "$") + word[3..-1]
+  end
+  array_new
+end
+
+
+def find_a(array)
+  array.select { |letter| letter.start_with?("a") }
+end
+
+
+def sum_array(array)
+  array.inject {|number, index| number += index; number}
+end
+
+
+def add_s(array)
+  array_new = []
+  array.each_with_index.collect do |word, index|
+    array_new << (word[index[0]].insert(-1, 's'))
+  end
+  array_new
+end
+
+# # Question 9
+# describe '#add_s' do
+#   it 'Add an "s" to each word in the array except for the 2nd element in the array' do
+#     expect(add_s(["hand","feet", "knee", "table"])).to eq(["hands","feet", "knees", "tables"])
+#   end
+# end
+# end
