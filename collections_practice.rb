f2b18f7c9b67dc3611af687aa2b2e6b2327dac76
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-21 02:11:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,6 +59,7 @@ def add_s(array)
   array.each_with_index.collect do |word, index|
     # array_new = (word.insert(-1, 's'))
     if word.include?("feet")
+      array[1].1.times do name.chop!
     else
       array_new = "#{word}".insert(-1, 's')
 
