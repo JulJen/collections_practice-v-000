3f0190a9cf71e430885477f6d3199fd942f8c916
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-21 02:19:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,7 +59,7 @@ def add_s(array)
 
   array.each_with_index.collect do |word, index|
     # array_new = (word.insert(-1, 's'))
-  array_new << word(remove_s) + ("#{word}".insert(-1, 's'))
+    array_new << word(remove_s) + ("#{word}".insert(-1, 's'))
 
   # end
   #   [array.tap(&:pop)].join(", ")
@@ -68,10 +68,10 @@ def add_s(array)
   # array.each_with_index.collect do |word, index|
   #   array_new = (word.insert(-1, 's')
     # array_new << (word[index[0]].insert(-1, 's'))
-    # end
-    # array_new
   end
+  array_new
 end
+
 
 # # Question 9
 # describe '#add_s' do
